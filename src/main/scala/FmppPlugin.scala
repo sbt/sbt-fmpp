09704b78624c4f2a50d59c49517d958c55fd8180
Alois Cochard <alois.cochard@gmail.com>
2012-02-12 22:30:40 +0100

    Basic scala-template processing implementation

--- src/main/scala/FmppPlugin.scala
+++ src/main/scala/FmppPlugin.scala
@@ -2,6 +2,7 @@ package fmpp
 
 import sbt._
 import Keys._
+import sbt.Fork
 
 import java.io.File
 
@@ -10,27 +11,49 @@ object FmppPlugin extends Plugin {
 
   val fmpp = TaskKey[Seq[File]]("fmpp", "Generate Scala sources from FMPP Scala Template")
   val fmppArgs = SettingKey[Seq[String]]("fmpp-args", "Extra command line parameters to FMPP.")
+  val fmppMain = SettingKey[String]("fmpp-main", "FMPP main class.")
   val fmppVersion =  SettingKey[String]("fmpp-version", "FMPP version.")
 
   lazy val fmppSettings = Seq[Project.Setting[_]](
     fmppArgs := Seq("--ignore-temporary-files"),
+    fmppMain := "fmpp.tools.CommandLine",
     fmppVersion := "0.9.14",
-    libraryDependencies in Fmpp <+= (fmppVersion in Fmpp)("net.sourceforge.fmpp" % "fmpp" % _ % Fmpp.name),
-    sourceDirectory in Fmpp <<= (sourceDirectory in Compile) { _ / "scala-fmpp" },
+    libraryDependencies <+= (fmppVersion in Fmpp)("net.sourceforge.fmpp" % "fmpp" % _ % Fmpp.name),
+    sourceDirectory in Fmpp <<= (sourceDirectory in Compile) { _ / "scala-template" },
     scalaSource in Fmpp <<= (sourceManaged in Compile),
 
     managedClasspath in Fmpp <<= (classpathTypes, update) map { (ct, report) =>
       Classpaths.managedJars(Fmpp, ct, report)
     },
-    sources in Fmpp <<= (sourceDirectory in Fmpp) map (identity(_) :: Nil),
 
-    fmpp <<= (sources in Fmpp, sourceManaged in Fmpp, managedClasspath in Fmpp).map(process)
+    fmpp <<= (
+      sourceDirectory in Fmpp,
+      sourceManaged in Fmpp,
+      fmppMain in Fmpp,
+      fmppArgs in Fmpp,
+      managedClasspath in Fmpp,
+      javaHome,
+      streams
+    ).map(process)
   )
 
-  private def process(sources: Seq[File], sourceManaged: File, classpath: Classpath) = {
-    println(sources)
-    println(sourceManaged)
-    println(classpath.map(_.data))
+  private def process(
+    source: File,
+    sourceManaged: File,
+    mainClass: String,
+    args: Seq[String],
+    classpath: Classpath,
+    javaHome: Option[File],
+    streams: TaskStreams
+  ) = {
+    Fork.java(
+      javaHome,
+      List(
+        "-cp", classpath.map(_.data).mkString(":"), mainClass,
+        "-S", source.toString, "-O", sourceManaged.toString
+      ) ::: args.toList,
+      streams.log
+    )
     (sourceManaged ** "*.scala").get
   }
 }
